5a7435869946847c86cfdc048dd4dbb5046f9c80
blh2666 <blh2666@g.rit.edu>
2021-04-29 00:34:40 -0400

    made tailwind css go over bootstrap to fix link color

--- www/settings.php
+++ www/settings.php
@@ -4,5 +4,7 @@ $BASE_DIRECTORY = dirname(__FILE__);
 $UPLOAD_DIRECTORY = dirname(__FILE__)."/mosaic_uploads";
 $ARCHIVE_DIRECTORY = dirname(__FILE__)."/mosaics";
 $SHARED_DRIVE_DIRECTORY = dirname(__FILE__)."/shared_drive";
-mkdir($SHARED_DRIVE_DIRECTORY);
+if (!file_exists($SHARED_DRIVE_DIRECTORY)) {
+    mkdir($SHARED_DRIVE_DIRECTORY);
+}
 ?>
